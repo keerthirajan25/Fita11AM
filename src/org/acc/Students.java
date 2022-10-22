35c0bd6a289937ae31a53a00abce1a7cfdc87e3d
keerthirajan25 <kraj25985@gmail.com>
2022-10-22 12:13:57 +0530

    task done

---
+++ src/org/acc/Students.java
@@ -0,0 +1,14 @@
+package org.acc;
+
+public class Students {
+	
+	public void add() {
+		// TODO Auto-generated method stub
+System.out.println("Addition");
+	}
+	public static void main(String[] args) {
+		Students s= new Students();
+		s.add();
+	}
+
+}
